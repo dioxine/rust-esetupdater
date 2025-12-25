4fa447add94787a35387e0f206b1896bc6c67932
DioXine <dioxine@mail.ru>
2025-12-25 18:00:46 +1000

    Fixed path sanitizing

--- src/internal/process.rs
+++ src/internal/process.rs
@@ -45,12 +45,7 @@ pub async fn process_ini(
                 .unwrap_or(format!("/{remote_custom_modules_dir}/").as_str()),
             info.file.trim_start_matches('/')
         );
-        println!("This is formed remote URL: {url}");
-
         let local_path = derive_local_path(&info.file, root_dir);
-
-        println!("This is formed local path: {local_path}");
-
         modified_ini_data.insert(
             section_name.to_string(),
             ModuleInfo {
